1bc76aea31ac7cf9ad6d3aa7684f3c93a43961e3
August Sandell <august.sandell@hotmail.se>
2019-05-16 16:29:50 +0200

    update chart.js

--- src/components/chartComponents/chart.js
+++ src/components/chartComponents/chart.js
@@ -1,15 +1,4 @@
-import * as React from 'react'
-
-/*export function makeFakeObject() {
-    
-    let obj = {
-        dict: {},
-        setAttribute: function(key, value) {
-            this.dict[key] = value;
-        }
-    };
-    return obj;
-}*/
+var animationDuration = 0.5;
 
 export function makeSvg(width,height) {
     let svg = document.createElementNS(`http://www.w3.org/2000/svg`, `svg`);
@@ -29,11 +18,10 @@ export function makeCircle(x,y,r,color){
     return circle;
 }
 
-export function makeSlice(xStart,yStart,xEnd,yEnd,percent,color){
+export function makeSlice(radius, xStart,yStart,xEnd,yEnd,percent,color){
     var slice = document.createElementNS(`http://www.w3.org/2000/svg`, `path`);
-
-    const xRadius = 1;
-    const yRadius = 1;
+    const xRadius = radius;
+    const yRadius = radius;
     const xAxisRotation = 0;
     const largeArcFlag = (percent <= 0.5 ? `0` : `1`);
     const sweepFlag = 1;
@@ -46,7 +34,7 @@ export function makeSlice(xStart,yStart,xEnd,yEnd,percent,color){
     return slice;
 }
 
-export function makeRectangle(width,height,x,y,color) {
+export function makeRectangle(x,y,width,height,color) {
     var box = document.createElementNS(`http://www.w3.org/2000/svg`, `rect`);
     box.setAttribute(`width`, width);
     box.setAttribute(`height`, height);
@@ -68,34 +56,53 @@ export function makeText(x,y,color) {
 
 export function makeLine(x1,y1,x2,y2,width,color){
     var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
     line.setAttribute("x1", x1);
     line.setAttribute("y1", y1);
     line.setAttribute("x2", x2);
     line.setAttribute("y2", y2);
+
     line.setAttribute("style", `stroke:${color};stroke-width:${width}`);
 
     return line;
 }
 
+export function makeAnimation(attributeName,from,to,animationDuration,delay){
+    var animate = document.createElementNS(`http://www.w3.org/2000/svg`, `animate`);
+    animate.setAttribute("attributeName",attributeName);
+    animate.setAttribute("from",from);
+    animate.setAttribute("to",to);
+    animate.setAttribute("dur",animationDuration);
+    animate.setAttribute("keyTimes","0;1");
+    animate.setAttribute("calcMode","spline");
+    //animate.setAttribute("keySplines",".65 .15 .35 .85");
+    animate.setAttribute("keySplines",".3 0 .3 1");
+    animate.setAttribute("begin",delay);
+    animate.setAttribute("fill","freeze");
+
+    return animate;
+}
+
 export function graphColors(amount){
-    let startColor = {r:0,g:71,b:122};
-    let endColor = {r:0,g:226,b:242};
 
-    let colors = [];
+    var startColor = {r:0,g:71,b:122};
+    var endColor = {r:0,g:226,b:242};
+
+    var colors = [];
     for(var i = 0; i < amount; i++){
-        let rColor = Math.round(startColor.r + (i+1)*((endColor.r-startColor.r)/(amount+1)));
-        let gColor = Math.round(startColor.g + (i+1)*((endColor.g-startColor.g)/(amount+1)));
-        let bColor = Math.round(startColor.b + (i+1)*((endColor.b-startColor.b)/(amount+1)));
+        var rColor = Math.round(startColor.r + (i+1)*((endColor.r-startColor.r)/(amount+1)));
+        var gColor = Math.round(startColor.g + (i+1)*((endColor.g-startColor.g)/(amount+1)));
+        var bColor = Math.round(startColor.b + (i+1)*((endColor.b-startColor.b)/(amount+1)));
 
-        let rHex = rColor.toString(16);
+        var rHex = rColor.toString(16);
         if(rHex.length < 2){
             rHex = `0` + rHex;
         }
-        let gHex = gColor.toString(16);
+        var gHex = gColor.toString(16);
         if(gHex.length < 2){
             gHex = `0` + gHex;
         }
-        let bHex = bColor.toString(16);
+        var bHex = bColor.toString(16);
         if(bHex.length < 2){
             bHex = `0` + bHex;
         }
@@ -109,57 +116,60 @@ export function makePiechart(width, height, pieDataPoints, doSort, showText){
     if(doSort){
         pieDataPoints.sort(function(a,b){return a.value-b.value}).reverse();
     }
-    var xStart;
-    var yStart;
-    var xEnd;
-    var yEnd;
-    var midAngle;
-
 
     var svg = makeSvg(width, height);
     svg.setAttribute(`viewBox`, `-1 -1 2 2`);
     svg.setAttribute(`style`, `transform: rotate(-0.25turn)`);
 
-    let angle = 0;
-    let valueSum = 0;
+    var radius = 1;
+    var angle = 0;
+    var valueSum = 0;
 
-    for(let pieDataPoint of pieDataPoints){
+    for(var pieDataPoint of pieDataPoints){
         valueSum += pieDataPoint.value;
     }
 
-    let colors = graphColors(pieDataPoints.length);
+    var colors = graphColors(pieDataPoints.length);
 
     for(var i = 0; i < pieDataPoints.length; i++){
-        let pieDataPoint = pieDataPoints[i];
+        pieDataPoint = pieDataPoints[i];
 
         if(i==0){
-            xStart = 0;
-            yStart = 1;
+            pieDataPoint.xStart = 0;
+            pieDataPoint.yStart = radius;
         }
         else{
-            xStart = xEnd;
-            yStart = yEnd;
+            pieDataPoint.xStart = pieDataPoints[i-1].xEnd;
+            pieDataPoint.yStart = pieDataPoints[i-1].yEnd;
         }
-
-        let thisAngle = 2*Math.PI*(pieDataPoint.value/valueSum);
+        var thisAngle = 2*Math.PI*(pieDataPoint.value/valueSum)
         angle += thisAngle;
-        yEnd = Math.cos(angle);
-        xEnd = Math.sin(angle);
+        pieDataPoint.midAngle = angle - thisAngle/2;
+        pieDataPoint.yEnd = Math.cos(angle) * radius;
+        pieDataPoint.xEnd = Math.sin(angle) * radius;
 
-        svg.appendChild(makeSlice(xStart,yStart,xEnd,yEnd,pieDataPoint.value/valueSum, colors[i]));   
-    
-        if(showText){
-            let radius = 0.7;
-            midAngle = angle - thisAngle/2;
-            let x=radius*Math.cos(midAngle);
-            let y=radius*Math.sin(midAngle);
+        var slice = makeSlice(radius, pieDataPoint.xStart,pieDataPoint.yStart,pieDataPoint.xEnd,pieDataPoint.yEnd,pieDataPoint.value/valueSum, colors[i]);
+        
+        slice.appendChild(makeAnimation("d",makeSlice(0, 0,0,0,0,pieDataPoint.value/valueSum, colors[i]).getAttribute("d"),slice.getAttribute("d"),animationDuration,0));
+
+        svg.appendChild(slice);
+
+        
+        if(showText) {
+            var textRadius = 0.7;
+            var midAngle = angle - thisAngle/2;
+            var x=textRadius*Math.cos(midAngle);
+            var y=textRadius*Math.sin(midAngle);
+            var textSize = height/2000;
 
             var text = makeText(x,y,"white");
 
             text.setAttribute(`text-anchor`,`middle`);
-            text.setAttribute(`style`, `font: normal ${height/2000}px sans-serif`);
+            text.setAttribute(`style`, `font: normal ${0}px sans-serif`);
             text.setAttribute(`transform`, `rotate(90 ${x} ${y})`);
             text.innerHTML = pieDataPoints[i].value;
+            text.appendChild(makeAnimation("font-size",0,textSize,animationDuration/2,animationDuration/2));
+            
 
             svg.appendChild(text);
         }
@@ -179,9 +189,9 @@ export function makeBarChart(totalWidth,height,barDataPoints,doSort,showText){
     var textSize = (totalWidth+height)/40;
     var textSpace = -(height/40);
     var marginTop = showText ? 0.8 : 0.95;
-    
     var biggestValue = 0;
-    for(let i = 0; i < barAmount; i++){
+    
+    for(i = 0; i < barAmount; i++){
         if(barDataPoints[i].value > biggestValue)
         {
             biggestValue = barDataPoints[i].value;
@@ -192,7 +202,6 @@ export function makeBarChart(totalWidth,height,barDataPoints,doSort,showText){
     if(biggestValue/5 < 1){
         marginLeft = Math.ceil(Math.abs(Math.log10(biggestValue/5)) + 2)*0.035;
     }
-
     var width = totalWidth - marginLeft*totalWidth;
     var f = height / biggestValue;
     f = f * marginTop;
@@ -227,7 +236,7 @@ export function makeBarChart(totalWidth,height,barDataPoints,doSort,showText){
         var spaceWidth = (width * spacePercent) / (barAmount + 1)
     }
 
-    for(let i = 0; i < barAmount; i++) {
+    for(var i = 0; i < barAmount; i++) {
         let barHeight = f * barDataPoints[i].value;
 
         let barXPos = (i + 1) * spaceWidth + (i * barWidth) + marginLeft*totalWidth;
@@ -238,10 +247,16 @@ export function makeBarChart(totalWidth,height,barDataPoints,doSort,showText){
             valueText.innerHTML = barDataPoints[i].value; 
             valueText.setAttribute(`text-anchor`, `middle`); 
             valueText.setAttribute(`style`, `font: normal ${textSize}px arial`);  
+            valueText.appendChild(makeAnimation("y",height + textSpace, (barYPos + textSpace), animationDuration,0));
             svg.appendChild(valueText);
         }
+
+        var rect = makeRectangle(barXPos, barYPos, barWidth, barHeight, colors[i]);
+                
+        rect.appendChild(makeAnimation("height",0, barHeight, animationDuration,0));
+        rect.appendChild(makeAnimation("y",height, height - barHeight, animationDuration,0));
         
-        svg.appendChild(makeRectangle(barWidth, barHeight, barXPos, barYPos, colors[i]));
+        svg.appendChild(rect);        
     }
 
     return svg;
@@ -255,21 +270,21 @@ export function makeHorizontalBarChart(width, height, barHorizontalData, doSort,
         barHorizontalData.sort(function(a,b){
             var aWidth = 0;
             var bWidth = 0;
-            for(i = 0; i < a.barWidth.length; i++){
+            for(var i = 0; i < a.barWidth.length; i++){
                 aWidth += a.barWidth[i];
             }
-            for(i = 0; i < b.barWidth.length; i++){
+            for(var i = 0; i < b.barWidth.length; i++){
                 bWidth += b.barWidth[i];
             }
             return aWidth - bWidth;
         }).reverse();
     }
 
-    let barsPercent = 0.5;
-    let spacePercent = 1-barsPercent;
+    var barsPercent = 0.5;
+    var spacePercent = 1-barsPercent;
 
-    let barHeight = (barsPercent*height)/barHorizontalData.length;
-    let spaceHeight = (spacePercent*height)/(barHorizontalData.length+1);
+    var barHeight = (barsPercent*height)/barHorizontalData.length;
+    var spaceHeight = (spacePercent*height)/(barHorizontalData.length+1);
     
     var biggestTotalValue = 0;
 
@@ -283,17 +298,18 @@ export function makeHorizontalBarChart(width, height, barHorizontalData, doSort,
         }
     }
 
-    console.log(`Biggest total value: ` + biggestTotalValue);
     var f = width / biggestTotalValue;
 
     var currentYPos = spaceHeight;
     for (var i = 0; i < barHorizontalData.length; i++) {
         var currentXPos = 0;
-        var colors = graphColors(barHorizontalData[i].barWidth.length);
+        var numberOfSections = barHorizontalData[i].barWidth.length
+        var colors = graphColors(numberOfSections);
 
         for (let j = 0; j < barHorizontalData[i].barWidth.length; j++) {
             
-            var rectangle = makeRectangle(barHorizontalData[i].barWidth[j] * f, barHeight, currentXPos, currentYPos, colors[j]);
+            var barWidth = barHorizontalData[i].barWidth[j];
+            var rectangle = makeRectangle(currentXPos,currentYPos,barWidth * f, barHeight, colors[j]);
             svg.appendChild(rectangle);
             
             if (showText){
@@ -311,68 +327,126 @@ export function makeHorizontalBarChart(width, height, barHorizontalData, doSort,
         currentYPos += (spaceHeight + barHeight);
     }
 
+    var animationRectangle = makeRectangle(0,0,width,height,"white");
+    animationRectangle.appendChild(makeAnimation("width",width,0,animationDuration,0));
+    animationRectangle.appendChild(makeAnimation("x",0,width,animationDuration,0));
+    svg.appendChild(animationRectangle);
+
     return svg;
 }
 
 export function makeLineChart(width, height, lines){
-    let svg = makeSvg(width,height);
-    let colors = graphColors(lines.length);
+    var svg = makeSvg(width,height);
+    var colors = graphColors(lines.length);
 
-    let xMargin = 0.1*width;
-    let yMargin = 0.1*height;
+    var xMargin = 0.1*width;
+    var yMargin = 0.1*height;
+    var circleSize = height/40;
 
-    let topCoord = topCoord(lines);
-    let xFactor = (width-xMargin)/topCoord.x;
-    let yFactor = (height-yMargin)/topCoord.y;
+    var topCoord = getTopCoord(lines);
+    var xFactor = (width-xMargin)/topCoord.x;
+    var yFactor = (height-yMargin)/topCoord.y;
 
-    for(let i = 0; i < lines.length; i++){
+    for(var i = 0; i < lines.length; i++){
         var line = lines[i];
 
-        svg.appendChild(makeCircle(xMargin/2 + line[0].x*xFactor, height - yMargin/2 - line[0].y*yFactor, height/40, colors[i]));
+        animationDuration /= (line.length-1);
+
+        let circle = makeCircle(xMargin/2 + line[0].x*xFactor, height - yMargin/2 - line[0].y*yFactor, 0, colors[i]);
+        circle.appendChild(makeAnimation("r", 0, circleSize, animationDuration/5,0));
+        svg.appendChild(circle);
         
-        for(let j = 1; j < line.length; j++){
-            let coordPrev = line[j-1];
-            let coordCurr = line[j];
+        for(var j = 1; j < line.length; j++){
+            var coordPrev = line[j-1];
+            var coordCurr = line[j];
 
-            let x1 = xMargin/2 + coordPrev.x*xFactor;
-            let y1 = height - yMargin/2 - coordPrev.y*yFactor;
-            let x2 = xMargin/2 + coordCurr.x*xFactor;
-            let y2 = height - yMargin/2 - coordCurr.y*yFactor;
+            var x1 = xMargin/2 + coordPrev.x*xFactor;
+            var y1 = height - yMargin/2 - coordPrev.y*yFactor;
+            var x2 = xMargin/2 + coordCurr.x*xFactor;
+            var y2 = height - yMargin/2 - coordCurr.y*yFactor;
 
-            svg.appendChild(makeCircle(x2, y2, height/40, colors[i]));
+            let circle = makeCircle(x2, y2, 0, colors[i])
+            circle.appendChild(makeAnimation("r", 0, circleSize, animationDuration/5,animationDuration*(j)));
+            svg.appendChild(circle);
 
-            svg.appendChild(makeLine(x1,y1,x2,y2,height/50,colors[i]));
+            let lineWidth = height/50;
+            let svgLine = makeLine(x1,y1,x1,y1,lineWidth,colors[i]);
+            svgLine.appendChild(makeAnimation("x2",x1,x2,animationDuration,animationDuration*(j-1)));
+            svgLine.appendChild(makeAnimation("y2",y1,y2,animationDuration,animationDuration*(j-1)));
+            svg.appendChild(svgLine);
         }
+
+        animationDuration *= (line.length-1);
+    }
+
+    return svg;    
+}
+
+export function makeAreaGraph(width, height, lines){
+    var svg = makeSvg(width,height);
+    var colors = graphColors(lines.length);
+
+    var xMargin = 0.1*width;
+    var yMargin = 0.1*height;
+
+    var topCoord = getTopCoord(lines);
+
+    var xFactor = (width-xMargin)/topCoord.x;
+    var yFactor = (height-yMargin)/topCoord.y;
+
+    for(var i = 0; i < lines.length; i++){
+        var line = lines[i];
+
+        var area = document.createElementNS(`http://www.w3.org/2000/svg`, `path`);
+
+        var pathStringTo = `M 0 ${height}`
+        var pathStringFrom = `M 0 ${height}`
+    
+        
+        
+        for(var j = 0; j < line.length; j++){
+            var y = height - line[j].y*yFactor
+            pathStringTo += ` L ${line[j].x*xFactor} ${y}`
+            pathStringFrom += ` L ${line[j].x*xFactor} ${height}`
+        }
+
+        pathStringTo += ` L ${line[line.length-1].x*xFactor} ${height}`
+        pathStringFrom += ` L ${line[line.length-1].x*xFactor} ${height}`
+
+
+        area.setAttribute(`d`,pathStringFrom);
+        area.setAttribute(`fill`, colors[i]);
+        area.appendChild(makeAnimation("d", pathStringFrom, pathStringTo, animationDuration/2, (animationDuration/2)/lines.length*i));
+
+        svg.appendChild(area);
     }
     return svg;
 }
 
 export function backgroundLines(svg,topY,width,height,marginTop,marginRight,marginBottom,marginLeft){
-    let yFactor = (height-marginTop-marginBottom)/topY;
-    console.log("height",height-marginTop-marginBottom);
-    let yStepGoal = topY/5;
-    let yStep10 = Math.pow(10,Math.round(Math.log10(yStepGoal)));
-    let yStep = yStep10;
+    var yFactor = (height-marginTop-marginBottom)/topY;
+    var yStepGoal = topY/5;
+    var yStep10 = Math.pow(10,Math.round(Math.log10(yStepGoal)));
+    var yStep = yStep10;
 
     if(Math.abs(yStepGoal - yStep10/5) < Math.abs(yStepGoal - yStep10)){yStep = yStep10/5;}
     if(Math.abs(yStepGoal - yStep10/2) < Math.abs(yStepGoal - yStep10)){yStep = yStep10/2;}
     if(Math.abs(yStepGoal - yStep10*2) < Math.abs(yStepGoal - yStep10)){yStep = yStep10*2;}
     if(Math.abs(yStepGoal - yStep10*5) < Math.abs(yStepGoal - yStep10)){yStep = yStep10*5;}
 
-    console.log("yStep",yStep);
-    let numberOfLines = topY/yStep;
-    let numberOfSpaces = numberOfLines+1;
-    for(let i = 0; i < numberOfSpaces; i++){
-        let y = height-marginBottom - yStep*i*yFactor;
+    var numberOfLines = topY/yStep;
+    var numberOfSpaces = numberOfLines+1;
+    for(var i = 0; i < numberOfSpaces; i++){
+        var y = height-marginBottom - yStep*i*yFactor;
         
         if(y<height*0.003){break;}
         
         let text = makeText(width*marginLeft*0.8,y,`#bbbbbb`);
-        let textNumber = yStep*i;
+        var textNumber = yStep*i;
 
         if(textNumber < 1){
-            let numberOfDecimals = Math.ceil(Math.abs(Math.log10(yStep)));
-            let roundingFactor = Math.pow(10,numberOfDecimals);
+            var numberOfDecimals = Math.ceil(Math.abs(Math.log10(yStep)));
+            var roundingFactor = Math.pow(10,numberOfDecimals);
 
             textNumber = Math.round(textNumber*roundingFactor)/roundingFactor;
         }
@@ -392,11 +466,11 @@ export function backgroundLines(svg,topY,width,height,marginTop,marginRight,marg
     return svg;
 }
 
-export function topCoord(lines){
-    let topX = 0;
-    let topY = 0;
-    for(let line of lines){
-        for(let coord of line){
+export function getTopCoord(lines){
+    var topX = 0;
+    var topY = 0;
+    for(var line of lines){
+        for(var coord of line){
             if(coord.x > topX){topX = coord.x}
             if(coord.y > topY){topY = coord.y}
         }
@@ -404,26 +478,3 @@ export function topCoord(lines){
 
     return {x:topX,y:topY};
 }
-
-
-//  Classes
-
-
-/*class BarDataPoint {
-    name = "";
-    value = "";
-    BarDataPoint = (name,value) => {
-        this.name = name;
-        this.value = value;
-    }
-}
-
-function PieDataPoint(name, value){
-    this.name = name;
-    this.value = value;
-    this.xStart;
-    this.yStart;
-    this.xEnd;
-    this.yEnd;
-    this.midAngle;
-}*/
