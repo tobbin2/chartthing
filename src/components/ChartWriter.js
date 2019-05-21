af407da90b4d9618d2b63c1dceb22c5fb9539768
August Sandell <august.sandell@hotmail.se>
2019-05-21 15:48:06 +0200

    Revert "Merge remote-tracking branch 'origin/alyssa2' into augell"
    This reverts commit 64f179d352a89156a451da5808e2f5fc83700e75, reversing
    changes made to 2769f06a3fbf6f42dfb5ad6d200af12710e28453.

--- src/components/ChartWriter.js
+++ src/components/ChartWriter.js
@@ -7,14 +7,12 @@ import { SampleTextComponent } from './chartComponents/SampleTextComponent'
 import { LineComponent } from './chartComponents/LineComponent'
 import { BarComponentChart } from './chartComponents/BarComponentCharts' 
 import { HorizontalStackerBarComponent } from './chartComponents/HorizontalStackedBarComponent'
-import { AccidentComponent} from './chartComponents/AccidentComponent'
 
 import { DoughnutComponentChart } from './otherChartComponents/DoughnutComponentChart'
 import { RadarComponentChart } from './otherChartComponents/RadarComponentChart'
 import { ArrowComponent } from './otherChartComponents/ArrowComponent'
 import { PolarareaComponentChart } from "./otherChartComponents/PolarareaComponentChart"
 import { GradientLineComponentChart } from './otherChartComponents/GradientLineComponentChart'
-
 
 const data = require('./object2.json')
 
@@ -99,8 +97,6 @@ export class ChartWriter extends React.Component {
                 return <BarComponentChart data={data} />
             case "horizontalStackedStaple":
                 return <HorizontalStackerBarComponent data={data} />
-            case "accident":
-                return <AccidentComponent data={data} />
             default:
                 return
         }
